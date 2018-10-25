7ffc56c27360d6ac5f15aaa0469dea6e932d5822
Felix <koehlerfelix@arcor.de>
2018-10-25 11:43:57 +0200

    add main

--- Lego/src/main/Main.java
+++ Lego/src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
-public class Main {
-	
-	public static void main(String[] args) {
-		
-		
-	}
+import robot.Robot;
+import mission.TaskMenu;
 
+public class Main {
+
+	public static void main(String[] Args) {
+		
+		Robot robot = new Robot();
+		TaskMenu menu = new TaskMenu();
+		menu.showMenu();
+	}
 }
