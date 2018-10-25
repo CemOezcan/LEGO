3ddc56cc87a48edba0e66267fe0016a3bbab32a5
Felix <koehlerfelix@arcor.de>
2018-10-25 11:31:22 +0200

    added context

--- Lego/src/mission/TaskMenu.java
+++ Lego/src/mission/TaskMenu.java
@@ -1,5 +1,24 @@
 package mission;
 
-public class TaskMenu {
+import lejos.utility.TextMenu;
+import mission.Task;
 
+public class TaskMenu extends TextMenu {
+
+	public TaskMenu(String[] items) {
+		super(items);
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	public TaskMenu() {
+		super(Task.getTaskList());
+	}
+	*/
+	
+	public void showMenu() {
+		
+	}
+	
 }
+
