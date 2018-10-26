78862986971e64002fc01e22dae64ceeb9fc50c4
Cem Özcan <uvtwy@student.kit.edu>
2018-10-26 23:11:56 +0200

    implements basic movement

--- Lego/src/robot/Robot.java
+++ Lego/src/robot/Robot.java
@@ -6,8 +6,11 @@ import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.robotics.RegulatedMotor;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class Robot {
+	
+	private DifferentialPilot pilot;
 
 	private final Port MOTOR_LEFT = MotorPort.C;
 	private final Port MOTOR_Right = MotorPort.D;
@@ -18,37 +21,50 @@ public class Robot {
 	private final Port SENSOR_TOUCH_RIGHT = SensorPort.S3;
 	private final Port SENSOR_ULTRA_SONIC = SensorPort.S4;
 	
+	private RegulatedMotor motorRight = new EV3LargeRegulatedMotor(MOTOR_Right);
+	private RegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MOTOR_LEFT);
+	private RegulatedMotor motorUltraSonic = new EV3MediumRegulatedMotor(MOTOR_LEFT);
 	
-	RegulatedMotor motorRight = new EV3LargeRegulatedMotor(MOTOR_Right);
-	RegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MOTOR_LEFT);
-	RegulatedMotor motorUltraSonic = new EV3MediumRegulatedMotor(MOTOR_LEFT);
 	
 	public Robot() {
+		//TODO: real values
+		double rightWheel = 0;
+		double leftWheel = 0;
+		double trackWidth = 0;
+		boolean reverse = false;
+		
+		this.pilot = new DifferentialPilot(leftWheel, rightWheel, trackWidth, this.motorLeft, this.motorRight, reverse);
 		
 	}
 	
 	public void forward() {
-		
+		this.pilot.stop();
+		this.pilot.forward();
 	}
 	
 	public void backward() {
-		
+		this.pilot.stop();
+		this.pilot.backward();
 	}
 	
 	public void turnLeft() {
-		
+		this.pilot.stop();
+		this.pilot.steer(200, 90);
 	}
 	
 	public void turnRight() {
-		
+		this.pilot.stop();
+		this.pilot.steer(-200, 90);
 	}
 	
-	public void RotateLeft(int degree) {
-		
+	public void RotateLeft(double degree) {
+		this.pilot.stop();
+		this.pilot.steer(200, degree);
 	}
 	
-	public void RotateRight(int degree) {
-		
+	public void RotateRight(double degree) {
+		this.pilot.stop();
+		this.pilot.steer(-200, degree);
 	}
 	
 }
