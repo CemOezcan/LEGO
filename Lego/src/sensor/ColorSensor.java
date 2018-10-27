38fb071b5bd171e9fafa8f4ae5966b854670b028
Cem Özcan <uvtwy@student.kit.edu>
2018-10-27 02:17:37 +0200

    Implement ColorSensor-methods

--- Lego/src/sensor/ColorSensor.java
+++ Lego/src/sensor/ColorSensor.java
@@ -2,6 +2,7 @@ package sensor;
 
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
 
 public class ColorSensor extends Sensor {
 	
@@ -10,22 +11,33 @@ public class ColorSensor extends Sensor {
 	public ColorSensor(Port port) {
 		super(port);
 		this.sensor = new EV3ColorSensor(this.port);
+		this.setAmbientMode();
 	}
 	
 	public void setClorIDMode() {
 		this.switchMode(this.sensor, "ColorID");
+		this.sensor.setFloodlight(true);
 	}
 	
 	public void setRedMode() {
 		this.switchMode(this.sensor, "Red");
+		this.sensor.setFloodlight(Color.RED);
 	}
 	
 	public void setRGBMode() {
 		this.switchMode(this.sensor, "RGB");
+		this.sensor.setFloodlight(Color.WHITE);
 	}
 	
 	public void setAmbientMode() {
 		this.switchMode(this.sensor, "Ambient");
+		this.sensor.setFloodlight(false);
+	}
+	
+	public float[] getColor() {
+		float[] sample = new float[this.sensor.sampleSize()];
+		this.sensor.fetchSample(sample, 0);
+		return sample;
 	}
 	
 }
