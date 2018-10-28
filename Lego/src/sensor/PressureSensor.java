e5a688585d164e8c889dda54dd60f248587c54ab
Cem Özcan <uvtwy@student.kit.edu>
2018-10-28 23:55:30 +0100

    Implement touch detection

--- Lego/src/sensor/PressureSensor.java
+++ Lego/src/sensor/PressureSensor.java
@@ -9,12 +9,20 @@ public class PressureSensor extends AnalogSensor {
 	
 	public PressureSensor(Port port) {
 		super(port);
+		
 		this.sensor = new EV3TouchSensor(this.port);
 		this.switchMode(this.sensor.getTouchMode());
 	}
 	
+	/**
+	 * 
+	 * @return true, exactly when sensor detects touch 
+	 */
 	public boolean isTouched() {
-		return true;
+		float[] sample = new float[this.sampleProvider.sampleSize()];
+		this.sampleProvider.fetchSample(sample, 0);
+		
+		return (sample[0] == 0) ? (false) : (true);
 	}
 	
 }
