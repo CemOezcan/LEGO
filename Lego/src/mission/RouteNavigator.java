ce8c0444ff7ea150f4700a54dc4c373ea4a0e3b7
Felix <koehlerfelix@arcor.de>
2018-11-03 14:02:25 +0100

    Implemets first try of a p regulator

--- Lego/src/mission/RouteNavigator.java
+++ Lego/src/mission/RouteNavigator.java
@@ -14,7 +14,12 @@ public class RouteNavigator implements Mission {
 	private final float BLACK = 0.08f;
 	private final float WHITE = 0.48f;
 	private final float BLUE = 0.0f; //TODO: add value
-	private final float OFFSET = 0.1f; 
+	private final float OFFSET = 0.01f; 
+	private final float OPTIMALVALUE = (WHITE + BLACK) / 2;
+	
+	private final float tempo = 220f;
+	
+	private final float kp = (tempo / WHITE - OPTIMALVALUE);
 
 	public RouteNavigator(Robot robot) {
 		this.robot = robot;
@@ -29,16 +34,40 @@ public class RouteNavigator implements Mission {
 		robot.forward();
 		
 		boolean nextDirection = true;
+		boolean end = false;
 		
-		while (Button.LEFT.isUp()) {
+		float lastDifference = 0f;
+		
+		while (Button.LEFT.isUp() && !end) {
 			
-			if (robot.getColorSensor().getColor()[0] < 0.3) {
-				nextDirection = this.align(nextDirection, 100);
+			float leftMotorSpeed = 0;
+			float rightMotorSpeed = 0;
+			
+			float actualValue = robot.getColorSensor().getColor()[0];
+			
+			//the touchsensors are touched and the robot has to drive around the obstacle
+			if (robot.getPressureSensorLeft().isTouched() || robot.getPressureSensorRight().isTouched()) {
+				driveAroundObstacle();
+				
+			} else if (actualValue > WHITE - 2 * OFFSET) { //90 degree turn
+				
+			} else if (actualValue < BLACK + OFFSET) { //LineGap
+				findLineAfterGap();
+			} else { //normal case
+				lastDifference = actualValue - OPTIMALVALUE;
+				float y = kp * lastDifference;
+				
+				leftMotorSpeed = tempo - y;;
+				rightMotorSpeed = tempo + y;
+				
+				//RobotMotorGEschwindigkeit anpassen mit den übergebenen Werten left right Motor speed
+				//robot.setSpeed(leftMotorSpeed, rightMotorSpeed);
 			}
 		}
+		
 		robot.stop();
 	}
-	
+
 	public void driveAroundObstacle() {
 		Sound.beepSequence();
 	}
