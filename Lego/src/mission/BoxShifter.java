d05a83ada081ba8bcae010f3000313bdf45e4e69
Alexander Schorn <Alexander.Schorn@gmx.net>
2018-11-12 10:51:23 +0100

    Adjust algorithm

--- Lego/src/mission/BoxShifter.java
+++ Lego/src/mission/BoxShifter.java
@@ -9,70 +9,70 @@ import robot.Robot;
 public class BoxShifter implements Mission {
 
 	private final Robot robot;
-	
+
 	public BoxShifter(Robot robot) {
 		this.robot = robot;
 	}
-	
+
 	@Override
 	public void executeMission() {
 		Sound.beep();
-		while (Button.ENTER.isUp()) {
-			for (int i = 0; i < 100; i++) {
-				
-				float actualSonicValue = 0.0f;
-				
-				this.robot.getColorSensor().setRedMode();
-				
-				// find box
-				this.robot.drawString("find box", 0, 0);
-				
-				this.robot.RotateLeft(1100);
-				this.robot.backward();
-				
-				actualSonicValue = this.robot.getUltraSonicSensor().getDistance();
-				
-				while(actualSonicValue > 0.2) {
-					actualSonicValue = this.robot.getUltraSonicSensor().getDistance();
-					this.robot.drawString("" + actualSonicValue, 0, 0);
-				}
-				
-				this.robot.motorsStop();
-				
-				// push box to wall
-				this.robot.drawString("push Box to wall", 0, 0);
-				
-				this.robot.RotateLeft(550);
-				
-//				this.robot.forward();
-//				while(!robot.getPressureSensorLeft().isTouched() && !robot.getPressureSensorRight().isTouched()) {
-//					robot.drawString("Left: " + robot.getPressureSensorLeft().isTouched(), 0, 0);
-//					robot.drawString("Right: " + robot.getPressureSensorRight().isTouched(), 0, 10);
-//				}
-				
-				this.robot.pilotTravel(20);
-				
-				// push box to edge
-				this.robot.drawString("push Box to edge", 0, 0);
-				
-				this.robot.RotateLeft(550);
-				this.robot.pilotTravel(7);
-				this.robot.RotateRight(550);
-				this.robot.pilotTravel(7);
-				this.robot.RotateRight(550);
-				
-				this.robot.pilotTravel(20);
-				
-//				this.robot.forward();
-				
-//				while(!robot.getPressureSensorLeft().isTouched() && !robot.getPressureSensorRight().isTouched()) {
-//					robot.drawString("Left: " + robot.getPressureSensorLeft().isTouched(), 0, 0);
-//					robot.drawString("Right: " + robot.getPressureSensorRight().isTouched(), 0, 10);
-//				}
-				robot.motorsStop();
-			}
-		}
-		
-	}
 
+		float actualSonicValue = 0.0f;
+
+		this.robot.getColorSensor().setRedMode();
+
+		// find box
+		this.robot.drawString("find box", 0, 0);
+
+		this.robot.pilotTravel(5);
+		this.robot.RotateLeft(1150);
+		this.robot.backward();
+
+		actualSonicValue = this.robot.getUltraSonicSensor().getDistance();
+
+		while (actualSonicValue > 0.17) {
+			actualSonicValue = this.robot.getUltraSonicSensor().getDistance();
+			this.robot.drawString("" + actualSonicValue, 0, 0);
+		}
+
+		// push box to wall
+		this.robot.drawString("push Box to wall", 0, 0);
+
+		this.robot.RotateLeft(550);
+
+		// this.robot.forward();
+		// while(!robot.getPressureSensorLeft().isTouched() &&
+		// !robot.getPressureSensorRight().isTouched()) {
+		// robot.drawString("Left: " +
+		// robot.getPressureSensorLeft().isTouched(), 0, 0);
+		// robot.drawString("Right: " +
+		// robot.getPressureSensorRight().isTouched(), 0, 10);
+		// }
+
+		this.robot.pilotTravel(23);
+
+		// push box to edge
+		this.robot.drawString("push Box to edge", 0, 0);
+
+		this.robot.pilotTravel(-2);
+		this.robot.RotateLeft(550);
+		this.robot.pilotTravel(7);
+		this.robot.RotateRight(550);
+		this.robot.pilotTravel(6);
+		this.robot.RotateRight(550);
+
+		this.robot.pilotTravel(15);
+
+		// this.robot.forward();
+
+		// while(!robot.getPressureSensorLeft().isTouched() &&
+		// !robot.getPressureSensorRight().isTouched()) {
+		// robot.drawString("Left: " +
+		// robot.getPressureSensorLeft().isTouched(), 0, 0);
+		// robot.drawString("Right: " +
+		// robot.getPressureSensorRight().isTouched(), 0, 10);
+		// }
+		robot.motorsStop();
+	}
 }
