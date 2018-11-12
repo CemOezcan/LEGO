6143fb83f41e78d7ed980e32479f8c0fc94bee1b
Alexander Schorn <Alexander.Schorn@gmx.net>
2018-11-12 10:26:46 +0100

    Add BoxShifter algorithm

--- Lego/src/mission/BoxShifter.java
+++ Lego/src/mission/BoxShifter.java
@@ -16,12 +16,60 @@ public class BoxShifter implements Mission {
 	
 	@Override
 	public void executeMission() {
-		// TODO Auto-generated method stub
 		Sound.beep();
-		Delay.msDelay(1000);
 		while (Button.ENTER.isUp()) {
 			for (int i = 0; i < 100; i++) {
-				LCD.drawString("BoxShifterAlgorithm", 0, 0);
+				
+				float actualSonicValue = 0.0f;
+				
+				this.robot.getColorSensor().setRedMode();
+				
+				// find box
+				this.robot.drawString("find box", 0, 0);
+				
+				this.robot.RotateLeft(1100);
+				this.robot.backward();
+				
+				actualSonicValue = this.robot.getUltraSonicSensor().getDistance();
+				
+				while(actualSonicValue > 0.2) {
+					actualSonicValue = this.robot.getUltraSonicSensor().getDistance();
+					this.robot.drawString("" + actualSonicValue, 0, 0);
+				}
+				
+				this.robot.motorsStop();
+				
+				// push box to wall
+				this.robot.drawString("push Box to wall", 0, 0);
+				
+				this.robot.RotateLeft(550);
+				
+//				this.robot.forward();
+//				while(!robot.getPressureSensorLeft().isTouched() && !robot.getPressureSensorRight().isTouched()) {
+//					robot.drawString("Left: " + robot.getPressureSensorLeft().isTouched(), 0, 0);
+//					robot.drawString("Right: " + robot.getPressureSensorRight().isTouched(), 0, 10);
+//				}
+				
+				this.robot.pilotTravel(20);
+				
+				// push box to edge
+				this.robot.drawString("push Box to edge", 0, 0);
+				
+				this.robot.RotateLeft(550);
+				this.robot.pilotTravel(7);
+				this.robot.RotateRight(550);
+				this.robot.pilotTravel(7);
+				this.robot.RotateRight(550);
+				
+				this.robot.pilotTravel(20);
+				
+//				this.robot.forward();
+				
+//				while(!robot.getPressureSensorLeft().isTouched() && !robot.getPressureSensorRight().isTouched()) {
+//					robot.drawString("Left: " + robot.getPressureSensorLeft().isTouched(), 0, 0);
+//					robot.drawString("Right: " + robot.getPressureSensorRight().isTouched(), 0, 10);
+//				}
+				robot.motorsStop();
 			}
 		}
 		
