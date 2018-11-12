3849490a33520aace10459b4679a9dc9a91d8aaf
Felix <koehlerfelix@arcor.de>
2018-11-12 18:15:14 +0100

    implement first try of the bridge algorithm

--- Lego/src/mission/BridgeCrosser.java
+++ Lego/src/mission/BridgeCrosser.java
@@ -1,12 +1,20 @@
 package mission;
 
+import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
+import robot.RegulatorP;
 import robot.Robot;
 
 public class BridgeCrosser implements Mission {
 
 	private final Robot robot;
+	
+	private final float tempo = 200f;
+	private final float ROBOTHEIGHT = 0.07f;
+	private final float BRIDGEHEIGHT = 0.3f;
+	private final float OPTIMALVALUE = (ROBOTHEIGHT + BRIDGEHEIGHT) / 2;
+	private final float KP = 1000;
 	
 	public BridgeCrosser(Robot robot) {
 		this.robot = robot;
@@ -14,10 +22,26 @@ public class BridgeCrosser implements Mission {
 	
 	@Override
 	public void executeMission() {
-		// TODO Auto-generated method stub
-		Sound.beep();
-		for (int i = 0; i < 10; i--) {
-			LCD.drawString("BridgeCrosserAlgorithm", 0, 0);
+		this.robot.beep();
+
+		this.robot.ultraSonicSensorDown();
+		this.robot.getColorSensor().setColorIDMode();
+		RegulatorP regulator = new RegulatorP(this.robot, this.tempo, this.KP, this.OPTIMALVALUE);
+
+		//this.robot.forward();
+
+		boolean end = false;
+		robot.adjustMotorspeed(tempo, tempo);
+
+		while (Button.LEFT.isUp() && !end) {
+			float actualGroundDistance = robot.getUltraSonicSensor().getDistance();
+			float actualColorValue = robot.getColorSensor().getColor()[0];
+			if (actualColorValue == 1 || actualColorValue == 2) {
+				end = true;
+			} else {
+				this.robot.drawString(" distance: " + actualGroundDistance, 0, 0);
+				regulator.sonicRegulate(actualGroundDistance);
+			}
 		}
 	}
 
