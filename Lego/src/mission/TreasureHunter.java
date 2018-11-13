88185c395ae034189f8987c2665d3f80644cf63f
Felix <koehlerfelix@arcor.de>
2018-11-13 13:22:50 +0100

    first try of the treasure hunter

--- Lego/src/mission/TreasureHunter.java
+++ Lego/src/mission/TreasureHunter.java
@@ -1,12 +1,18 @@
 package mission;
 
+import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
+import robot.RegulatorP;
 import robot.Robot;
 
 public class TreasureHunter implements Mission {
 
 	private final Robot robot;
+	
+	private final float tempo = 150f;
+	private final float[] red = {1,1}; //Points to find
+	private final float[] blue = {1,1};
 	
 	public TreasureHunter(Robot robot) {
 		this.robot = robot;
@@ -14,11 +20,34 @@ public class TreasureHunter implements Mission {
 	
 	@Override
 	public void executeMission() {
-		// TODO Auto-generated method stub
-		Sound.beep();
-		for (int i = 0; i < 10; i--) {
-			LCD.drawString("TreasureHunterAlgorithm", 0, 0);
+		this.robot.beep();
+
+		this.robot.getColorSensor().setRGBMode();
+		boolean foundBlue = false;
+		boolean foundRed = false;
+		robot.adjustMotorspeed(tempo, tempo);
+
+		
+		while (Button.LEFT.isUp() && !(foundRed && foundBlue)) {
+			robot.forward();
+			float[] actualColorValue = robot.getColorSensor().getColor();
+			boolean isTouched = (robot.getPressureSensorLeft().isTouched() || robot.getPressureSensorRight().isTouched());
+			if (actualColorValue == red) {
+				foundRed = true;
+				Sound.beepSequence();
+			} else if (actualColorValue == blue) {
+				foundBlue = true;
+				Sound.beepSequence();
+			}
+			if (isTouched) {
+				turn();
+			}
+		
 		}
+	}
+	
+	public void turn() {
+		
 	}
 
 }
