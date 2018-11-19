aa2e163a71611009d7d8b1067bf937f06a7e9545
Alexander Schorn <Alexander.Schorn@gmx.net>
2018-11-19 10:21:11 +0100

    add endSequence

--- Lego/src/mission/BridgeCrosser.java
+++ Lego/src/mission/BridgeCrosser.java
@@ -9,47 +9,59 @@ import robot.Robot;
 public class BridgeCrosser implements Mission {
 
 	private final Robot robot;
-	
+
 	private final float tempo = 300f;
 	private final float ROBOTHEIGHT = 0.03f;
 	private final float OPTIMALVALUE = 0.1f;
 	private final float KP = 2000;
 	private final float OFFSET = 0.03f;
-	
+
 	public BridgeCrosser(Robot robot) {
 		this.robot = robot;
 	}
-	
+
 	@Override
 	public boolean executeMission() {
 		this.robot.beep();
 		this.robot.getColorSensor().setColorIDMode();
+
 		RegulatorP regulator = new RegulatorP(this.robot, this.tempo, this.KP, this.OPTIMALVALUE);
 
 		boolean end = false;
 		robot.adjustMotorspeed(tempo, tempo);
 		this.robot.ultraSonicSensorDown();
-		while (Button.LEFT.isUp() && !end) {
+		while (Button.LEFT.isUp() && !end && !this.robot.getPressureSensorLeft().isTouched()) {
 			float actualGroundDistance = getDistanceValue(robot.getUltraSonicSensor().getDistance());
-			float actualColorValue = robot.getColorSensor().getColor()[0];
-			
-			//if (actualColorValue == 1 || actualColorValue == 2) {
-			//	end = true;
-			//} else {
-				this.robot.drawString(" distance: "  + actualGroundDistance, 0, 0);
-				regulator.bridgeRegulate(actualGroundDistance);
-			//}
-				
+			this.robot.drawString(" distance: " + actualGroundDistance, 0, 0);
+			regulator.bridgeRegulate(actualGroundDistance);
 		}
+		endSequence();
 		return end;
 	}
-	
+
 	public float getDistanceValue(float value) {
 		float result = value;
 		if (value > ROBOTHEIGHT + OFFSET) {
 			result = 0.2f;
 		}
 		return result;
+	}
+	
+	public void endSequence() {
+		this.robot.motorsStop();
+		this.robot.pilotTravel(-2);
+		this.robot.RotateRight(40);
+		
+		//find blue line
+		float actualColorValue = robot.getColorSensor().getColor()[0];
+		this.robot.forward();
+		
+		while(actualColorValue == 1 || actualColorValue == 2) {
+			
+		}
+		
+		
+		
 	}
 
 }
